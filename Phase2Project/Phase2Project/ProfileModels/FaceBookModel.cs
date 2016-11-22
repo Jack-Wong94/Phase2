6952b0010677e2ec46501a3de5fdcd6477293df4
Jack-Wong94 <lwon229@aucklanduni.ac.nz>
2016-11-22 21:43:25 +1300

    add azure package and basic structure of the app

---
+++ Phase2Project/Phase2Project/ProfileModels/FaceBookModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace Test6
+{
+    public class FaceBookModel
+    {
+        [JsonProperty(PropertyName = "Name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "id")]
+        public string id { get; set; }
+
+        [JsonProperty(PropertyName = "email")]
+        public string email { get; set; }
+
+        [JsonProperty(PropertyName = "gender")]
+        public string gender { get; set; }
+
+
+    }
+}
